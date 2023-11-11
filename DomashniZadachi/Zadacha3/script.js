1704023be48949216df0198322401b5da67f80e9
Ognen Sekuloski <ognensekuloski@gmail.com>
2023-11-11 17:26:03 +0100

    probi so js

--- DomashniZadachi/Zadacha3/script.js
+++ DomashniZadachi/Zadacha3/script.js
@@ -1,3 +1,38 @@
-document.writeln("<h1>Ova e proben tekst</h1>")
 
-zborovi = []
+const eightLetterWords = [
+    'Apple',
+    'Table',
+    'Nature',
+    'Yellow',
+    'Family',
+    'Country',
+    'Product',
+    'Morning',
+    'Library',
+    'Silence',
+    'Weather',
+    'Diamond',
+    'Freedom',
+    'Explore',
+    'Failure',
+    'Imagine',
+    'Journey',
+    'Perfect',
+    'Quality',
+    'Success'
+];
+
+    let currentWord = eightLetterWords[parseInt(Math.random()*eightLetterWords.length)];
+
+    
+    console.log(currentWord);
+
+    for (let index = 0; index < currentWord.length; index++) {
+        const element = currentWord[index];
+        console.log("Letter at index " + index + " is " + element);
+    }
+
+    console.log("***");
+    console.log(currentWord[parseInt(Math.random()*currentWord.length)]);
+    
+
