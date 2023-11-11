3a9a9141c85dc739901c688a894563698234dfa8
Ognen Sekuloski <ognensekuloski@gmail.com>
2023-11-11 21:28:22 +0100

    Stuff that works: -random word picking -picking 3 random letters -generating as many input boxes as there are letters in the word

--- DomashniZadachi/Zadacha3/script.js
+++ DomashniZadachi/Zadacha3/script.js
@@ -1,3 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
 
 const eightLetterWords = [
     'Apple',
@@ -22,9 +23,44 @@ const eightLetterWords = [
     'Success'
 ];
 
-    let currentWord = eightLetterWords[parseInt(Math.random()*eightLetterWords.length)];
 
+//picking a random word
+    let currentWord = eightLetterWords[parseInt(Math.random()*eightLetterWords.length)];
+//getting word length
+    // let wordLength = currentWord.length;
+
+//this will be the final array
+    let finalArray = new Array(currentWord.length).fill(0);
     
+//creating control array
+    let controlArray = new Array(currentWord.length);
+
+    for (let index = 0; index < currentWord.length; index++) {
+        controlArray[index] = [currentWord[index], 0];
+    }
+
+//choosing 3 random letters
+
+    let chosenCount = 0;
+
+    while(chosenCount < 3){
+        let randomIndex = parseInt(Math.random()*currentWord.length);
+
+        if (controlArray[randomIndex][1] == 0) {
+            finalArray[randomIndex] = currentWord[randomIndex];
+            controlArray[randomIndex][1] = 1;
+            chosenCount++;
+        }
+    }
+
+
+
+    for (let index = 0; index < currentWord.length; index++) {
+        const element = finalArray[index];
+        console.log(finalArray[index]);
+    }
+
+//testing
     console.log(currentWord);
 
     for (let index = 0; index < currentWord.length; index++) {
@@ -34,5 +70,34 @@ const eightLetterWords = [
 
     console.log("***");
     console.log(currentWord[parseInt(Math.random()*currentWord.length)]);
+
+    generateInputs(parseInt(currentWord.length));
+
+});
     
+
+
+//generating the correct number of inputs
+
+function generateInputs(numOfInputs) {
+    const inputCount = parseInt(numOfInputs);
+    
+    const inputContainer = document.getElementById('inputContainer');
+
+    if (!inputContainer) {
+        console.error("Error: 'inputContainer' element not found.");
+        return;
+    }
+
+    inputContainer.innerHTML = ''; // Clear existing inputs
+
+    for (let index = 0; index < inputCount; index++) {
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.id = 'LetterInput' + index;
+        input.maxLength = 1;
+        inputContainer.appendChild(input);
+    }
+}
+
 
