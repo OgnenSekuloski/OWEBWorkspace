a93b43f11ce6e6ba8cf5249e0078f67fce969930
Ognen Sekuloski <ognensekuloski@gmail.com>
2024-01-26 18:03:33 +0100

    Domashna zadacha 5

---
+++ DomashniZadachi/Zadacha5/src/db-data.ts
@@ -0,0 +1,44 @@
+export const DRIVERS: any = [
+  //undefined
+
+  {
+    id: 1,
+    name: 'Ognen Sekuloski',
+    iconUrl:
+      'https://img.asmedia.epimg.net/resizer/3kixINBSED2oBv6JOUpyTjKqB6Q=/736x414/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/NAI2CC5JMYXG5552LVJGMGG5GA.jpg',
+    description: 'Najbrz',
+    category: 'INTERMEDIATE',
+    lessonsCount: 410,
+    points: 450,
+  },
+  {
+    id: 2,
+    name: 'David Arsovski',
+    iconUrl:
+      'https://img.asmedia.epimg.net/resizer/db5wseQ4_uInNJIhPaH_2ZIYMy4=/736x414/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/7GRTQT6C3I5EUE5ILE5U5PBRZU.jpg',
+    description: 'Ekstra brz',
+    category: 'PRO',
+    lessonsCount: 310,
+    points: 400,
+  },
+  {
+    id: 3,
+    name: 'Damjan Mihajlovski',
+    iconUrl:
+      'https://img.asmedia.epimg.net/resizer/p97vj2fydECZyPHzavBKxRwW4QU=/736x414/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/FFZK2SQ3JJFCEVEEC7VDLHH3K4.jpg',
+    description: 'Posleden',
+    category: 'MAX',
+    lessonsCount: 210,
+    points: 330,
+  },
+  {
+    id: 3,
+    name: 'Mizimak',
+    iconUrl:
+      'https://img.asmedia.epimg.net/resizer/p97vj2fydECZyPHzavBKxRwW4QU=/736x414/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/FFZK2SQ3JJFCEVEEC7VDLHH3K4.jpg',
+    description: 'Ne znae da vozi i nema vozacka dozvola',
+    category: 'PRO',
+    lessonsCount: 210,
+    points: 10,
+  },
+];
